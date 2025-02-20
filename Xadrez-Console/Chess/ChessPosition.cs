d8776d0f5c7de0162b77518c1d6efd192772ae63
Thiago Evaristo <thiagoers@outlook.com>
2025-02-19 21:37:18 -0300

    Add PositionChess function

---
+++ Xadrez-Console/Chess/ChessPosition.cs
@@ -0,0 +1,26 @@
+﻿using Board;
+
+namespace Chess
+{
+    class ChessPosition
+    {
+        public int Row { get; set; }
+        public char Column { get; set; }
+
+        public ChessPosition(char column, int row)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        public Position ToPosition()
+        {
+            return new Position(8 - Row, Column - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + Column + Row;
+        }
+    }
+}
--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -8,21 +8,26 @@ namespace Xadrez_Console
     {
         static void Main(string[] args)
         {
-            try
-            {
-                ChessBoard chessBoard = new ChessBoard(8, 8);
-                chessBoard.PutPieceOn(new King(chessBoard, Color.Black), new Position(0, 4));
-                chessBoard.PutPieceOn(new Tower(chessBoard, Color.Black), new Position(1, 2));
-                chessBoard.PutPieceOn(new Tower(chessBoard, Color.Black), new Position(0, 8));
+            //try
+            //{
+            //    ChessBoard chessBoard = new ChessBoard(8, 8);
+            //    chessBoard.PutPieceOn(new King(chessBoard, Color.Black), new Position(0, 4));
+            //    chessBoard.PutPieceOn(new Tower(chessBoard, Color.Black), new Position(1, 2));
+            //    chessBoard.PutPieceOn(new Tower(chessBoard, Color.Black), new Position(0, 9));
 
-                BoardScreen.ShowBoard(chessBoard);
+            //    BoardScreen.ShowBoard(chessBoard);
+            //}
+            //catch (BoardException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
 
-                Console.ReadLine();
-            }
-            catch (BoardException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+            ChessPosition cp = new ChessPosition('e', 5);
+
+            Console.WriteLine(cp);
+            Console.WriteLine(cp.ToPosition());
+
+            Console.ReadLine();
         }
     }
 }
